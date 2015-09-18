7090be21d88d15cd84e9b16b4cfc84efced8349f
Torben Knerr <mail@tknerr.de>
2015-09-18 07:30:35 +0200

    add a test for JAVA_HOME being set and be more specific when testing for the installed java version

--- cookbooks/vm/spec/integration/recipes/java_spec.rb
+++ cookbooks/vm/spec/integration/recipes/java_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe 'vm::java' do
-  it 'installs java' do
-    expect(command('java -version').exit_status).to eq 0
+
+  it 'installs Oracle Java 8 update 31' do
+    expect(command('java -version').stderr).to include '1.8.0_31'
+  end
+  it 'sets JAVA_HOME correctly' do
+    expect(command('bash -l -c "echo \$JAVA_HOME"').stdout).to include '/usr/lib/jvm/java-8-oracle-amd64'
   end
 end
