383a22e306177cf8c73cbe4db81b428670caf714
Torben Knerr <mail@tknerr.de>
2015-09-18 12:44:48 +0200

    maven actually is in the PATH already (and the 'setup_bin' attribute is a noop) we just need to test for it in a new login shell

--- cookbooks/vm/recipes/maven.rb
+++ cookbooks/vm/recipes/maven.rb
@@ -1,7 +1,6 @@
 
 # install maven
 node.set['maven']['version'] = '3'
-node.set['maven']['setup_bin'] = true
 node.set['maven']['install_java'] = false
 node.set['maven']['3']['version'] = '3.1.1'
 node.set['maven']['3']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
--- cookbooks/vm/spec/integration/recipes/maven_spec.rb
+++ cookbooks/vm/spec/integration/recipes/maven_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'vm::maven' do
 
   it 'installs Maven 3.1.1' do
-    expect(command('mvn --version').stdout).to include 'Apache Maven 3.1.1'
+    expect(command('bash -l -c "mvn --version"').stdout).to include 'Apache Maven 3.1.1'
   end
 end
