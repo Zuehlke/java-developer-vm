ce2247bb69caad339a80d0137ebfa8a730139657
Torben Knerr <mail@tknerr.de>
2015-09-18 11:04:44 +0200

    specify the direct download URL instead of the mirror chooser so the download can be cached using ETag

--- cookbooks/vm/recipes/eclipse.rb
+++ cookbooks/vm/recipes/eclipse.rb
@@ -1,8 +1,9 @@
 
 # install eclipse
 node.set['eclipse']['version'] = 'mars'
-node.set['eclipse']['release_code'] = 'R'
-node.set['eclipse']['arch'] = 'x86_64'
-node.set['eclipse']['suite'] = 'jee'
+# use the URL directly so it can be better cached (-> etag)
+node.set['eclipse']['url'] = 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
+# don't install any plugins yet
 node.set['eclipse']['plugins'] = []
+
 include_recipe 'eclipse'
