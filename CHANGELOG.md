7804e33d742b1dc32cb6211d999aee9855512e31
Torben Knerr <mail@tknerr.de>
2017-05-02 15:07:37 +0200

    update to Java 8 u131

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@
 The initial release of the Java Developer VM, including:
 
  * ChefDK 1.3.32
- * Oracle JDK 8u31
+ * Oracle JDK 8u131
  * Maven 3.5.0
  * Eclipse Mars 4.5
 
--- cookbooks/vm/recipes/java.rb
+++ cookbooks/vm/recipes/java.rb
@@ -3,6 +3,6 @@
 node.override['java']['jdk_version'] = '8'
 node.override['java']['install_flavor'] = 'oracle'
 node.override['java']['oracle']['accept_oracle_download_terms'] = true
-node.override['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz'
-node.override['java']['jdk']['8']['x86_64']['checksum'] = '173e24bc2d5d5ca3469b8e34864a80da'
+node.override['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
+node.override['java']['jdk']['8']['x86_64']['checksum'] = '62b215bdfb48bace523723cdbb2157c665e6a25429c73828a32f00e587301236'
 include_recipe 'java'
