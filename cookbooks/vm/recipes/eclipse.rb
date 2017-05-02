c5565b158c7db9f0ea0be959f3984544bb96e441
Torben Knerr <mail@tknerr.de>
2017-05-02 14:07:17 +0200

    use node.override instead of node.set to get rid of deprectaion warnings

--- cookbooks/vm/recipes/eclipse.rb
+++ cookbooks/vm/recipes/eclipse.rb
@@ -1,9 +1,9 @@
 
 # install eclipse
-node.set['eclipse']['version'] = 'mars'
+node.override['eclipse']['version'] = 'mars'
 # use the URL directly so it can be better cached (-> etag)
-node.set['eclipse']['url'] = 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
+node.override['eclipse']['url'] = 'http://ftp.heanet.ie/pub/eclipse/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk-x86_64.tar.gz'
 # don't install any plugins yet
-node.set['eclipse']['plugins'] = []
+node.override['eclipse']['plugins'] = []
 
 include_recipe 'eclipse'
