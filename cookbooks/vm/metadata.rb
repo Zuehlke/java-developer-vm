55cceb6bebc41085d1e6b1a18461a3f4d4a47b39
Torben Knerr <mail@tknerr.de>
2017-05-02 12:45:21 +0200

    add missing issues and source URL in metadata

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -3,7 +3,10 @@ maintainer 'Torben Knerr'
 maintainer_email 'mail@tknerr.de'
 license 'MIT'
 description 'Installs/Configures a Java Developer VM'
+long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version '0.1.0'
+issues_url 'https://github.com/Zuehlke/java-developer-vm/issues'
+source_url 'https://github.com/Zuehlke/java-developer-vm'
 
 depends 'apt', '2.8.2'
 depends 'java', '1.32.0'
