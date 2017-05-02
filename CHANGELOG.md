887ecd0bd4b57fddd7a00650011844698b6e282a
Torben Knerr <mail@tknerr.de>
2017-05-02 15:05:58 +0200

    update to maven 3.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ The initial release of the Java Developer VM, including:
 
  * ChefDK 1.3.32
  * Oracle JDK 8u31
- * Maven 3.1.1
+ * Maven 3.5.0
  * Eclipse Mars 4.5
 
 See the [README](README.md) for more details.
--- cookbooks/vm/recipes/maven.rb
+++ cookbooks/vm/recipes/maven.rb
@@ -1,7 +1,7 @@
 
 # install maven
-node.override['maven']['version'] = '3.1.1'
-node.override['maven']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
-node.override['maven']['checksum'] = '077ed466455991d5abb4748a1d022e2d2a54dc4d557c723ecbacdc857c61d51b'
+node.override['maven']['version'] = '3.5.0'
+node.override['maven']['url'] = 'http://apache.mirrors.tds.net/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz'
+node.override['maven']['checksum'] = 'beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034'
 
 include_recipe 'maven'
