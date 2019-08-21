948a1875900a55538ed2826594da5b039247161d
Torben Knerr <mail@tknerr.de>
2019-08-21 22:04:17 +0200

    fix path to README in metadata.rb

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Torben Knerr'
 maintainer_email 'mail@tknerr.de'
 license 'MIT'
 description 'Installs/Configures a Java Developer VM'
-long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 issues_url 'https://github.com/Zuehlke/java-developer-vm/issues'
 source_url 'https://github.com/Zuehlke/java-developer-vm'
