549631442745d207aa6a7343ab82b9747a035e98
Torben Knerr <mail@tknerr.de>
2019-08-22 01:01:37 +0200

    set M2_HOME and MAVEN_OPTS env vars

--- cookbooks/vm/recipes/maven.rb
+++ cookbooks/vm/recipes/maven.rb
@@ -5,3 +5,10 @@ ark 'maven' do
   checksum '2528c35a99c30f8940cc599ba15d34359d58bec57af58c1075519b8cd33b69e7'
   has_binaries ['bin/mvn']
 end
+
+file '/etc/profile.d/maven.sh' do
+  content <<~EOF
+    export M2_HOME=/usr/local/maven
+    export MAVEN_OPTS="-Dmaven.repo.local=$HOME/.m2/repository -Xmx384m"
+    EOF
+end
--- cookbooks/vm/spec/integration/recipes/maven_spec.rb
+++ cookbooks/vm/spec/integration/recipes/maven_spec.rb
@@ -5,4 +5,12 @@ describe 'vm::maven' do
   it 'installs Maven 3.6.1' do
     expect(command('bash -l -c "mvn --version"').stdout).to include 'Apache Maven 3.6.1'
   end
+
+  it 'sets M2_HOME correctly' do
+    expect(command('bash -l -c "echo -n \$M2_HOME"').stdout).to eq '/usr/local/maven'
+  end
+
+  it 'sets MAVEN_OPTS to configure memory settings' do
+    expect(command('bash -l -c "echo \$MAVEN_OPTS"').stdout).to contain '-Xmx384m'
+  end
 end
