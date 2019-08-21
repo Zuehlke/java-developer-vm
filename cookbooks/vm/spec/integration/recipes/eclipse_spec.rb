befcdc19140d682c36b49569341adac489537393
Torben Knerr <mail@tknerr.de>
2019-08-22 00:52:48 +0200

    fix wording in test name

--- cookbooks/vm/spec/integration/recipes/eclipse_spec.rb
+++ cookbooks/vm/spec/integration/recipes/eclipse_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'vm::eclipse' do
 
-  it 'installs Eclipse 2019-06 (4.12)' do
+  it 'installs Eclipse 4.12 (2019-06)' do
     expect(file('/usr/local/eclipse')).to be_linked_to '/usr/local/eclipse-2019-06'
     expect(file('/usr/local/eclipse/.eclipseproduct').content).to contain 'version=4.12'
   end
